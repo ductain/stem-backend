b99ea28ae44fa7f7133635459b93654874aec074
ductain <taindse170080@fpt.edu.vn>
2024-02-06 00:27:32 +0700

    remove swagger css

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const swaggerJsDoc = require('swagger-jsdoc')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
-const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+
 const options = {
 	definition: {
 		openapi: "3.0.0",
@@ -29,7 +29,7 @@ const specs = swaggerJsDoc(options);
 const app = express();
 app.use(cors())
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs, { customCssUrl: CSS_URL }));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 const provinceRoute = require("./routes/Province");
 const schoolRoute = require("./routes/School");
