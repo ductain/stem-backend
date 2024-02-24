ee092430c87f7ca4c56a15f5d1cd68bc2812b2b8
ductain <taindse170080@fpt.edu.vn>
2024-02-24 22:36:46 +0700

    fix login 3

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const specs = swaggerJsDoc(options);
 
 const app = express();
 app.use(cors({
-	origin: 'http://localhost:5173/student',
+	origin: 'http://localhost:5173',
 	credentials: true,
 	optionsSuccessStatus: 200,
 	methods: "GET,POST,PUT,DELETE",
