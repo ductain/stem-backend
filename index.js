263757a9b8db3953b87d8528232d8ebae1fb708a
ductain <taindse170080@fpt.edu.vn>
2024-02-25 09:21:06 +0700

    fix cor

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ const specs = swaggerJsDoc(options);
 const app = express();
 app.use(cors({
 	origin: 'http://localhost:5173',
-	credentials: true,
-	optionsSuccessStatus: 200,
-	methods: "GET,POST,PUT,DELETE",
+	// credentials: true,
+	// optionsSuccessStatus: 200,
+	// methods: "GET,POST,PUT,DELETE",
 }))
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs, {customCssUrl: CSS_URL}));
