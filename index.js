ee4f10e88adacbc7c8502f9b3455ada6c492a0c1
ductain <taindse170080@fpt.edu.vn>
2024-02-25 10:01:15 +0700

    cookie

--- index.js
+++ index.js
@@ -1,65 +1,81 @@
 const express = require("express");
-const swaggerUI = require('swagger-ui-express')
-const swaggerJsDoc = require('swagger-jsdoc')
-const dotenv = require('dotenv').config()
-const cors = require('cors')
+const session = require("cookie-session");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
+const dotenv = require("dotenv").config();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
-const session = require("express-session");
 const passport = require("passport");
 const passportSetup = require("./passport");
-const CSS_URL = " https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
+const CSS_URL =
+  " https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css";
 const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Library API",
-			version: "1.0.0",
-			description: "A simple Express Library API",
-		},
-		servers: [
-			{
-				url: 'https://stem-backend.vercel.app/',
-			},
-			{
-				url: 'http://localhost:5000',
-			},
-		],
-	},
-	apis: ["./routes/*.js"],
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express Library API",
+    },
+    servers: [
+      {
+        url: "https://stem-backend.vercel.app/",
+      },
+      {
+        url: "http://localhost:5000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
 };
 const specs = swaggerJsDoc(options);
 
 const app = express();
-app.use(cors({
-	origin: 'http://localhost:5173',
-	// credentials: true,
-	// optionsSuccessStatus: 200,
-	// methods: "GET,POST,PUT,DELETE",
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    // credentials: true,
+    // optionsSuccessStatus: 200,
+    // methods: "GET,POST,PUT,DELETE",
+  })
+);
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs, {customCssUrl: CSS_URL}));
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(specs, { customCssUrl: CSS_URL })
+);
 
-const crypto = require("crypto");
-const secret = crypto.randomBytes(32).toString("hex");
-const salt = crypto.randomBytes(32).toString("hex");
-const salt2 = crypto.randomBytes(16).toString("hex");
-const salt3 = crypto.randomBytes(16).toString("hex");
-const hash = crypto.createHmac("sha256", salt, salt2, salt3);
-hash.update(secret);
-const hashedSecret = hash.digest("hex");
+// const crypto = require("crypto");
+// const secret = crypto.randomBytes(32).toString("hex");
+// const salt = crypto.randomBytes(32).toString("hex");
+// const salt2 = crypto.randomBytes(16).toString("hex");
+// const salt3 = crypto.randomBytes(16).toString("hex");
+// const hash = crypto.createHmac("sha256", salt, salt2, salt3);
+// hash.update(secret);
+// const hashedSecret = hash.digest("hex");
+
+// app.use(
+//   session({
+//     secret: hashedSecret,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { maxAge: 24 * 60 * 60 * 1000 },
+//   })
+// );
+
 app.use(
   session({
-    secret: hashedSecret,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { maxAge: 24 * 60 * 60 * 1000 },
+    name: "session",
+    keys: ["key1"],
+    maxAge: 24 * 60 * 60 * 1000,
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-const authRoute = require('./routes/Auth')
+const authRoute = require("./routes/Auth");
 const provinceRoute = require("./routes/Province");
 const schoolRoute = require("./routes/School");
 const studentRoute = require("./routes/Student");
@@ -71,7 +87,7 @@ const labRoute = require("./routes/Lab");
 const tutorialRoute = require("./routes/Tutorial");
 const groupRoute = require("./routes/Group");
 app.use(express.json());
-app.use("/auth", authRoute)
+app.use("/auth", authRoute);
 app.use("/province", provinceRoute);
 app.use("/school", schoolRoute);
 app.use("/student", studentRoute);
