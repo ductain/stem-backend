47af8671483e5f10f3e9dd33c78dda591a949e35
ductain <taindse170080@fpt.edu.vn>
2024-02-26 16:56:35 +0700

    add auth controller

---
+++ controllers/auth.js
@@ -0,0 +1,35 @@
+const jwt = require("jsonwebtoken");
+
+const loginSuccess = (req, res) => {
+  if (req?.user) {
+    const token = jwt.sign({ user: req.user }, "rommel");
+    res.cookie("token", token, { httpOnly: true });
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req?.user,
+    });
+  }
+};
+
+const loginFailed = (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "failure",
+  });
+};
+
+const logout = (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("http://localhost:5173");
+  });
+};
+
+module.exports = {
+    loginSuccess: loginSuccess,
+    loginFailed: loginFailed,
+    logout: logout
+}
--- routes/Auth.js
+++ routes/Auth.js
@@ -1,37 +1,16 @@
 const router = require("express").Router();
 const passport = require("passport");
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const { loginSuccess, loginFailed, logout } = require("../controllers/auth");
 // const CLIENT_URL = process.env.NODE_ENV === "production" ? "https://stem-dun.vercel.app/student" : "http://localhost:5173/student";
 // const HOME_URL = process.env.NODE_ENV === "production" ? "https://stem-dun.vercel.app/" : "http://localhost:5173";
 
 const CLIENT_URL = "http://localhost:5173/student"
 const HOME_URL = "http://localhost:5173"
 
-router.get("/login/success", (req, res) => {
-  if (req?.user) {
-    const token = jwt.sign({ user: req.user }, 'rommel');
-    res.cookie('token', token, { httpOnly: true });
-    res.status(200).json({
-      success: true,
-      message: "successfull",
-      user: req?.user,
-    });
-  }
-});
-router.get("/login/failed", (req, res) => {
-  res.status(401).json({
-    success: false,
-    message: "failure",
-  });
-});
-router.get("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect(HOME_URL);
-  });
-});
+router.get("/login/success", loginSuccess);
+router.get("/login/failed", loginFailed);
+router.get("/logout", logout);
 router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
 router.get(
   "/google/callback",
