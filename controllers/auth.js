d7785dab029b803ffae9b82059c6a3f47cf02f20
ductain <taindse170080@fpt.edu.vn>
2024-02-28 21:45:05 +0700

    add timeout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,15 @@
 const jwt = require("jsonwebtoken");
 
 const loginSuccess = (req, res) => {
+  const timeout = 60000;
+
+  // Create a timer that will respond with a timeout error
+  const timeoutTimer = setTimeout(() => {
+    res.status(504).json({
+      success: false,
+      message: "Request timed out",
+    });
+  }, timeout);
   if (req?.user) {
     const token = jwt.sign({ user: req.user }, "rommel");
     res.cookie("token", token, { httpOnly: true });
@@ -10,6 +19,8 @@ const loginSuccess = (req, res) => {
       user: req?.user,
     });
   }
+  // Clear the timeout timer if the response has been sent
+  clearTimeout(timeoutTimer);
 };
 
 const loginFailed = (req, res) => {
