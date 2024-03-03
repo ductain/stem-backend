a638b651a5126f5e074a19ef4bf933fc538b19e2
ductain <taindse170080@fpt.edu.vn>
2024-03-03 20:07:47 +0700

    fix domain

--- routes/Auth.js
+++ routes/Auth.js
@@ -31,7 +31,7 @@ router.get("/google/callback", (req, res, next) => {
       sameSite: 'none',
       secure: true,
       maxAge: 24 * 60 * 60 * 1000,
-      domain: 'stem-backend.vercel.app'
+      domain: 'https://stem-backend.vercel.app'
     });
 
     // Redirect to the desired page
