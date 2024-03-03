6a6c7072970cd88e44bff69df897a5d2f1ad60a8
ductain <taindse170080@fpt.edu.vn>
2024-03-03 20:09:34 +0700

    delete domain

--- routes/Auth.js
+++ routes/Auth.js
@@ -4,13 +4,16 @@ const { loginSuccess, loginFailed, logout } = require("../controllers/auth");
 // const CLIENT_URL = process.env.NODE_ENV === "production" ? "https://stem-dun.vercel.app/student" : "http://localhost:5173/student";
 // const HOME_URL = process.env.NODE_ENV === "production" ? "https://stem-dun.vercel.app/" : "http://localhost:5173";
 
-const CLIENT_URL = "http://localhost:5173/student"
-const HOME_URL = "http://localhost:5173"
+const CLIENT_URL = "http://localhost:5173/student";
+const HOME_URL = "http://localhost:5173";
 
 router.get("/login/success", loginSuccess);
 router.get("/login/failed", loginFailed);
 router.get("/logout", logout);
-router.get("/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 // router.get(
 //   "/google/callback",
 //   passport.authenticate("google", {
@@ -21,17 +24,16 @@ router.get("/google", passport.authenticate("google", { scope: ["profile", "emai
 router.get("/google/callback", (req, res, next) => {
   passport.authenticate("google", (err, { user, token }) => {
     if (err) {
-      res.redirect('/auth/login/failed')
+      res.redirect("/auth/login/failed");
     }
 
     // Set the JWT token as a cookie
     res.cookie("token", token, {
       httpOnly: true, // Make the cookie accessible only via HTTP(S)
       path: "/",
-      sameSite: 'none',
+      sameSite: "none",
       secure: true,
       maxAge: 24 * 60 * 60 * 1000,
-      domain: 'https://stem-backend.vercel.app'
     });
 
     // Redirect to the desired page
